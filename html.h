cce4d37950c6dd1a760ea00a9963e23f9f84fc5b
sumitsonule09 <sumitsonule.ds24@sbjit.edu.in>
2025-02-18 12:41:40 +0530

    Add files via upload

---
+++ html.h
@@ -0,0 +1,17 @@
+<html>
+<head>
+<h1>Sumit Sonule</h1>
+<h2>USN: CD24055</h2>
+</head>
+<body>
+<table>
+<table border="2">
+<tr>
+<th>Hobby</th>
+<th>favorite actress</th>
+</th>favorite Gun</th>
+</tr>
+<table>
+<tr>
+<td>chutiyappa karna</td>
+<td>
